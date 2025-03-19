c3f7b4d1b60e78b4d5af74586a3b46d859dbcb27
Vishnudhat Natarajan <48808713+vishn9893@users.noreply.github.com>
2025-03-19 10:38:13 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -102,6 +102,8 @@ const CONFIG = {
     'ArgoCD',
     'Prometheus',
     'Grafana',
+    'SonarQube',
+    'Verracode',
     'Cloud Computing (AWS, Azure, GCP)',
   ],
   experiences: [
@@ -166,11 +168,11 @@ const CONFIG = {
     // },
   ],
   // Display articles from your medium or dev account. (Optional)
-  // blog: {
-  //   source: 'dev', // medium | dev
-  //   username: 'arifszn', // to hide blog section, keep it empty
-  //   limit: 2, // How many articles to display. Max is 10.
-  // },
+  blog: {
+  source: 'medium', // medium | dev
+  username: 'VISHNU B.E', // to hide blog section, keep it empty
+  limit: 2, // How many articles to display. Max is 10.
+  },
   // googleAnalytics: {
   //   id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   // },
